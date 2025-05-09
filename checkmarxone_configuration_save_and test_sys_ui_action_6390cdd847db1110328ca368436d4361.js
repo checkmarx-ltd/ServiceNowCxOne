a9a72dc353856534de5080e496d02c1639f9cc18
cx-apo-sin <206053824+cx-apo-sin@users.noreply.github.com>
2025-05-09 11:24:50 +0530

    Update checkmarxone_configuration_save_and test_sys_ui_action_6390cdd847db1110328ca368436d4361.js
    Addition of other option to ingest custom state and to avoid special character in custom state.
    Maximum custom field user can enter is 1000 separated by ';'.

--- checkmarxone_configuration_save_and test_sys_ui_action_6390cdd847db1110328ca368436d4361.js
+++ checkmarxone_configuration_save_and test_sys_ui_action_6390cdd847db1110328ca368436d4361.js
@@ -24,6 +24,9 @@ function clientValidation() {
     } else if (g_form.getValue("filter_project") == "by_Id" && g_form.getValue("list_of_project_id_s").split(";").length > 1000) {
         g_form.addWarningMessage(getMessage("Project id's list is too long."));
         return false;
+    } else if (g_form.getValue("other_result_states").split(";").length > 1000) {
+        g_form.addWarningMessage(getMessage("Custom State's list is too long."));
+        return false;
     } else if (g_form.getValue("filter_project") == "by_name" && g_form.getValue("project_filter_by_name").split(";").length > 1000) {
         g_form.addWarningMessage(getMessage("Project Name's list is too long."));
         return false;
@@ -32,8 +35,18 @@ function clientValidation() {
         return false;
     }
 
-    if (g_form.getValue("import_sast") == "false" && g_form.getValue("import_sca") == "false" && g_form.getValue("import_kics") == "false") {
-        g_form.addWarningMessage(getMessage("Select SCA, SAST or IaC to proceed."));
+    if (g_form.getValue("import_sast") == "false" && g_form.getValue("import_sca") == "false" && g_form.getValue("import_kics") == "false" &&
+        g_form.getValue("include_ossf_scorecard") == "false" && g_form.getValue("include_secret_detection") == "false" && g_form.getValue("include_api_security") == "false") {
+        g_form.addWarningMessage(getMessage("Select SCA, SAST, IaC, OSSF Scorecard, API Security, Secret Detection to proceed."));
+        return false;
+    }
+
+    if (g_form.getValue("include_api_security") == 'true' && g_form.getValue("import_sast") == "false") {
+        g_form.addWarningMessage(getMessage('Select SAST also if API Security is Selected.'));
+        return false;
+    }
+    if (g_form.getValue("other_result_states") && g_form.getValue("other_result_states").match(/[^a-zA-Z0-9;]/)) {
+        g_form.addWarningMessage(getMessage("Allow only English alphabetic characters and numbers in custom state name"));
         return false;
     }
     gsftSubmit(null, g_form.getFormElement(), 'Checkmarxone_configuration_save');
@@ -46,6 +59,25 @@ function serverSide() {
     current.doRedirect = true;
     current.update();
     action.setRedirectURL(current);
+
+    // Handle Other result state
+    var resultStates = current.getValue("result_states") || "";
+    resultStates = resultStates.split(";").map(function (state) {
+        return state.trim();
+    }).join(",");
+    if (resultStates.includes("OTHER")) {
+        var otherResultStates = current.getValue("other_result_states") || "";
+        var otherStatesArray = otherResultStates.split(";").map(function (state) {
+            return state.trim().replace(/\s+/g, "_").toUpperCase();
+        });
+        resultStates = resultStates.replace("OTHER", otherStatesArray.join(","));
+        resultStates = resultStates.split(";").filter(function (state, index, self) {
+            return self.indexOf(state) === index;
+        }).join(",");
+        current.setValue("result_states", resultStates);
+    }
+    current.update();
+
     var instance = current.getValue("integration_instance");
     var implConfig = new GlideRecord("sn_sec_int_impl_config");
     implConfig.addQuery("implementation", instance);
@@ -58,6 +90,7 @@ function serverSide() {
         }
     }
     current.update();
+
     if (new CheckmarxOneConfigUtil().saveInstanceParams(current.getUniqueValue())) {
         var config = new GlideRecord("x_chec3_chexone_checkmarxone_configuration");
         if (config.get(current.getUniqueValue())) {
